77d41d24aef7dd3c4e4fc9bb8e591af9521e3af4
MauritiusChief <107936497+MauritiusChief@users.noreply.github.com>
2024-12-19 02:04:28 -0500

    枪械加成材料

---
+++ kubejs/client_scripts/script.js
@@ -0,0 +1,11 @@
+// priority: 0
+
+console.info('Hello, World! (You will see this line every time client resources reload)')
+
+onEvent("item.tooltip", tooltip => {
+    tooltip.add('tac:uncommon_material', '§a子弹伤害+1%')
+	tooltip.add('tac:rare_material', '§9子弹伤害+5%')
+	tooltip.add('tac:epic_material', '§5子弹伤害+25%')
+	tooltip.add('tac:legendary_material', '§6子弹伤害+100%')
+	tooltip.add('tac:ultimate_material', '§c子弹伤害+500%')
+})
